6c738a36cdf79d4e6b22cea686d9fe26f12b509f
LtSalt <pfefferundsalz@posteo.de>
2025-09-25 01:55:42 +0200

    soso transition try with setInterval

--- src/routes/areaY/+page.svelte
+++ src/routes/areaY/+page.svelte
@@ -10,20 +10,44 @@
 
 	const nMax = 192;
 	const year = new Tween(2011, { duration: 800, easing: cubicOut });
-	let year2 = $state(2011);
+	let year2 = $state(1990);
 
-	let tracker = $derived(counter.filter((d) => d.date <= year.current));
+	let tracker = $derived(counter.filter((d) => d.date <= year2));
 	let nPro = $derived(tracker.at(-1)['sum']);
 	let clipPath = $derived([
 		{ x: 192, y: 1988 },
 		{ x: 192, y: year.current }
 	]);
+
+	function addYear() {
+		year2++;
+		console.log('hey');
+	}
+
+	let interval = $state();
+
+	function interpolateValues() {
+		interval = setInterval(addYear, 100);
+
+		// for (let i = year2; i < 2020; i++) {
+		// 	setTimeout(addYear, 1000);
+		// }
+	}
+
+	$effect(() => {
+		if (year2 == 2011) clearInterval(interval);
+	});
+
+	$inspect(year2);
 </script>
 
-<button onclick={() => (year.target = 1988)}>1988</button>
-<button onclick={() => (year.target = 2000)}>2000</button>
-<button onclick={() => (year.target = 2011)}>2011</button>
-<button onclick={() => (year.target = 2025)}>2025</button>
+<input type="range" min="1988" max="2025" bind:value={year2} step="1" />
+<button onclick={interpolateValues}>Interpolate</button>
+
+<button onclick={() => (year2 = 1988)}>1988</button>
+<button onclick={() => (year2 = 2000)}>2000</button>
+<button onclick={() => (year2 = 2011)}>2011</button>
+<button onclick={() => (year2 = 2025)}>2025</button>
 
 <Plot
 	x={{ domain: [0, 192], label: false, axis: false }}
@@ -33,10 +57,12 @@
 	<clipPath id="clipPath">
 		<AreaX data={clipPath} x="x" y="y"></AreaX>
 	</clipPath>
-	<g clip-path="url(#clipPath)">
-		<Line data={counter} x="sum" y="date" />
-		<AreaX data={counter} x="sum" y="date" fill="blue" opacity="0.2" />
-		<Area data={counter} y1="date" x1="sum" x2={nMax} fill="red" opacity="0.2" />
+	<!-- <g clip-path="url(#clipPath)"> -->
+	<g>
+		<!-- <Line data={counter} x="sum" y="date" /> -->
+		<Line data={tracker} x="sum" y="date" style="transition: opacity 0.2s ease-in" />
+		<AreaX data={tracker} x="sum" y="date" fill="blue" opacity="0.2" />
+		<Area data={tracker} y1="date" x1="sum" x2={nMax} fill="red" opacity="0.2" />
 
 		<Line
 			data={[
